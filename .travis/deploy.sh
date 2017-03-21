8e364e4d5e204bdd0affe814037668a9d6946006
Demian <thde@users.noreply.github.com>
2017-03-21 15:23:15 +0100

    moved server path

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,4 +7,4 @@ eval "$(ssh-agent -s)"
 chmod 600 .travis/deploy_key.pem
 ssh-add .travis/deploy_key.pem
 
-scp -r dist tylr-web@web.teiler.io:/srv/http/web.teiler.io
+scp -r dist/* tylr-web@web.teiler.io:/srv/http/web.teiler.io
