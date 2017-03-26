d15c1a41a695948ecc61b2206ed241a64802eb29
tk-codes <kk.devpro11@gmail.com>
2017-03-26 22:05:10 +0200

    reactivate default npm test

--- karma.conf.js
+++ karma.conf.js
@@ -45,6 +45,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: process.env.TRAVIS ? ['Chrome_travis_ci'] : ['Chrome'],
-    singleRun: false
+    singleRun: process.env.TRAVIS ? true : false
   });
 };
