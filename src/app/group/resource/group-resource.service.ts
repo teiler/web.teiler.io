a1c8e4d8c081467487e1fe0d291e823bd338d607
Keerthikan@Home <kk.devpro11@gmail.com>
2017-03-30 01:53:48 +0200

    make post request to api/group/

--- src/app/group/resource/group-resource.service.ts
+++ src/app/group/resource/group-resource.service.ts
@@ -1,16 +1,32 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class GroupResourceService {
 
+  private readonly apiUrl = 'https://api.teiler.io/v1/';
+
   constructor(private http: Http) {
   }
 
-  public createGroup(name: string): Observable<string> {
-    console.log('resource service: should return GroupDTO', name);
-    return Observable.of<string>(`Create ${name}: waiting for tylr-api`);
+  public createGroup(name: string): Observable<any> {
+
+    const requestBody = {
+      name
+    };
+
+    return this.http.post(this.getRequesturl('group'), requestBody)
+      .map((response: Response) => {
+        console.log('resource service: api response - ', response);
+        return Observable.of<any>(response.json());
+      }).catch((error: any) => {
+        return Observable.of<any>(error);
+      });
+  }
+
+  private getRequesturl(endpoint: string): string {
+    return `${this.apiUrl}${endpoint}`;
   }
 
 }
