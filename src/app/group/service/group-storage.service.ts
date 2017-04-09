3bebbddb11433cd924547eab93aa1231a880942f
Keerthikan@Home <kk.devpro11@gmail.com>
2017-04-09 17:50:43 +0200

    limit recent groups to 3

--- src/app/group/service/group-storage.service.ts
+++ src/app/group/service/group-storage.service.ts
@@ -1,24 +1,29 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Group} from '../model/group';
 import {LogService} from 'app/core';
+import {GroupStorageAdapter} from '../model/group-storage-adapter';
 
 @Injectable()
 export class GroupStorageService {
   private readonly NAME = 'GroupStorageService';
   private readonly RECENT_GROUPS = 'recent-groups';
-  private readonly CURRENT_GROUP = 'current-group';
+  private readonly RECENT_GROUPS_SCHEMA = {key: 'recent-groups-schema', value: '1'};
+  private readonly LIMIT_RECENT_GROUPS = 3;
 
   private currentGroup: Group;
   public onCurrentGroupChanged: EventEmitter<Group> = new EventEmitter();
 
-  private recentGroups: Map<string, Group> = new Map();
-  public onRecentGroupsChanged: EventEmitter<Group[]> = new EventEmitter();
+  private recentGroups: Map<string, GroupStorageAdapter> = new Map();
+  public onRecentGroupsChanged: EventEmitter<GroupStorageAdapter[]> = new EventEmitter();
 
   constructor(private logService: LogService) {
-    if (!localStorage.getItem(this.RECENT_GROUPS)) {
-      this.setRecentGroups(this.recentGroups);
-    } else {
+    // check if schema migration is needed
+    if (localStorage.getItem(this.RECENT_GROUPS_SCHEMA.key) === this.RECENT_GROUPS_SCHEMA.value) {
       this.recentGroups = this.getRecentGroupsFromStorage();
+    } else {
+      localStorage.removeItem(this.RECENT_GROUPS);
+      localStorage.setItem(this.RECENT_GROUPS_SCHEMA.key, this.RECENT_GROUPS_SCHEMA.value);
+      this.setRecentGroups(this.recentGroups);
     }
   }
 
@@ -27,7 +32,7 @@ export class GroupStorageService {
     this.storeCurrentGroup(group);
 
     // update local storage
-    this.recentGroups.set(group.id, group);
+    this.recentGroups.set(group.id, new GroupStorageAdapter(group.id, group.name, group.fetchedTime));
     this.setRecentGroups(this.recentGroups);
   }
 
@@ -36,7 +41,7 @@ export class GroupStorageService {
     this.onCurrentGroupChanged.emit(this.currentGroup);
   }
 
-  public getRecentGroup(id: string): Group {
+  public getRecentGroup(id: string): GroupStorageAdapter {
     return this.recentGroups.get(id);
   }
 
@@ -61,31 +66,37 @@ export class GroupStorageService {
     }
   }
 
-  public getRecentGroups(): Group[] {
+  public getRecentGroups(): GroupStorageAdapter[] {
     const groupIterator = this.recentGroups.values();
     let mapItem = groupIterator.next();
-    const groups: Group[] = [];
+    const groups: GroupStorageAdapter[] = [];
     while (mapItem.value) {
-      groups.push(mapItem.value);
+      groups.push(GroupStorageAdapter.fromDto(mapItem.value));
       mapItem = groupIterator.next();
     }
-    return groups;
+    return this.sortRecentGroups(groups).slice(0, this.LIMIT_RECENT_GROUPS);
   }
 
-  private getRecentGroupsFromStorage(): Map<string, Group> {
+  private getRecentGroupsFromStorage(): Map<string, GroupStorageAdapter> {
     return this.stringToMap(localStorage.getItem(this.RECENT_GROUPS));
   }
 
-  private setRecentGroups(map: Map<string, Group>) {
+  private setRecentGroups(map: Map<string, GroupStorageAdapter>) {
     localStorage.setItem(this.RECENT_GROUPS, this.mapToString(map));
     this.onRecentGroupsChanged.emit(this.getRecentGroups());
   }
 
-  private mapToString(map: Map<string, Group>) {
+  private mapToString(map: Map<string, GroupStorageAdapter>) {
     return JSON.stringify(Array.from(map.entries()));
   }
 
-  private stringToMap(str: string): Map<string, Group> {
+  private stringToMap(str: string): Map<string, GroupStorageAdapter> {
     return new Map<string, Group>(JSON.parse(str));
+  }
+
+  private sortRecentGroups(groups: GroupStorageAdapter[]): GroupStorageAdapter[] {
+    return groups.sort((g1: GroupStorageAdapter, g2: GroupStorageAdapter) => {
+      return g2.fetchedTime.getTime() - g1.fetchedTime.getTime();
+    });
   }
 }
