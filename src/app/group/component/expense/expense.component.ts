d47d2b55e2462b2006c30a40c1ef4d721e3ce51f
tk-codes <kk.devpro11@gmail.com>
2017-04-24 14:19:49 +0200

    show people in ExpenseComponent

--- src/app/group/component/expense/expense.component.ts
+++ src/app/group/component/expense/expense.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {CrudOperation} from '../../model/crud-operation';
+import {Group} from '../../model/group';
 
 @Component({
   selector: 'tylr-expense',
@@ -9,15 +10,15 @@ import {CrudOperation} from '../../model/crud-operation';
 })
 export class ExpenseComponent implements OnInit {
   private MODE: CrudOperation;
+  public group: Group;
 
   constructor(private route: ActivatedRoute) {
     this.MODE = this.route.snapshot.paramMap.has('expenseId') ?
       CrudOperation.EDIT : CrudOperation.CREATE;
-    console.log('val', this.MODE);
   }
 
   ngOnInit() {
-
+    this.group = this.route.snapshot.data['group'];
   }
 
   test() {
