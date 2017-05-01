f2a0f667d1a14141b42e8dca1b1bc557d21e7d45
Keerthikan@Home <kk.devpro11@gmail.com>
2017-05-01 02:50:53 +0200

    show last compensations in dashboard as well

--- src/app/group/component/dashboard/dashboard.component.ts
+++ src/app/group/component/dashboard/dashboard.component.ts
@@ -6,6 +6,10 @@ import {GroupStorageService} from '../../service/group-storage.service';
 import {Subscription} from 'rxjs/Subscription';
 import {ExpenseService} from '../../service/expense.service';
 import {Expense} from '../../model/expense';
+import {Observable} from 'rxjs/Observable';
+import {CompensationService} from '../../service/compensation.service';
+import {Compensation} from '../../model/compensation';
+import {Transaction} from '../../model/transaction';
 
 @Component({
   selector: 'tylr-dashboard',
@@ -14,11 +18,12 @@ import {Expense} from '../../model/expense';
 })
 export class DashboardComponent implements OnInit {
   public group: Group;
-  public expenses: Expense[] = [];
+  public transactions: Transaction[] = [];
 
   constructor(private route: ActivatedRoute,
               private navigationService: NavigationService,
-              private expenseService: ExpenseService) {
+              private expenseService: ExpenseService,
+              private compensationService: CompensationService) {
   }
 
   ngOnInit() {
@@ -29,10 +34,31 @@ export class DashboardComponent implements OnInit {
       return;
     }
 
-    this.expenseService.getExpenses(this.group.id)
-      .subscribe(
-        (expenses: Expense[]) => {
-          this.expenses = expenses;
+    Observable.zip(
+      this.expenseService.getExpenses(this.group.id),
+      this.compensationService.getCompensations(this.group.id),
+      (expenses: Expense[], compensations: Compensation[]) => {
+        const transactions: Transaction[] = [];
+        expenses.forEach((expense: Expense) => {
+          transactions.push(expense);
         });
+        compensations.forEach((compensation: Compensation) => {
+          transactions.push(compensation);
+        });
+
+        transactions.sort((t1: Transaction, t2: Transaction) => {
+          return t2.modifiedTime.getTime() - t1.modifiedTime.getTime();
+        });
+        return transactions;
+      }
+    ).subscribe(
+      (transactions: Transaction[]) => {
+        this.transactions = transactions;
+      });
+  }
+
+  getTransactionEditLink(transaction: Transaction) {
+    const endpoint = (transaction instanceof Expense) ? 'expenses' : 'compensations';
+    return `${endpoint}/${transaction.id}/edit`;
   }
 }
