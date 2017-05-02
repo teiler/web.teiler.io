32faa2f1a6b0b72bc2585b0af52f641021e3a994
Lukas Röllin <lsroellin@gmail.com>
2017-05-02 11:08:38 +0200

    Better texts

--- src/app/shared/model/tylr-api-error.ts
+++ src/app/shared/model/tylr-api-error.ts
@@ -4,8 +4,8 @@ export const TylrApiError = {
   PAYER_NOT_FOUND: 'Payer is not found', // 404
   PROFITEER_NOT_FOUND: 'Profiteer is not found', // 404
   TRANSACTION_NOT_FOUND: 'Transaction is not found', // 404
-  SHARES_DONT_ADD_UP: 'Shared amounts don\'t sum up to the total amount', // 406
-  PEOPLE_NAME_CONFLICT: 'A group shouldn\'t have duplicate person names', // 409
+  SHARES_DONT_ADD_UP: 'Oh snap! That doesn\'t add up yet', // 406
+  PEOPLE_NAME_CONFLICT: 'Sorry, names in a group cannot be the same', // 409
   PAYER_AND_ROFITEER_CONFLICT: 'Payer and profiteer cannot be the same person', // 409
   PAYER_INACTIVE: 'Payer is not active', // 410
   PROFITEER_INACTIVE: 'Profiteer is not active', // 410
