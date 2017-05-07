3ad6f6240910c43ac1f64e5698938321c870b656
Keerthikan@Home <kk.devpro11@gmail.com>
2017-05-07 20:19:13 +0200

    fix e2e test and add person duplicate check

--- src/app/shared/model/tylr-web-error.ts
+++ src/app/shared/model/tylr-web-error.ts
@@ -3,5 +3,6 @@ export const TylrWebError = {
   EMPTY_EXPENSE_ID: 'Expense ID is empty',
   EMPTY_GROUP_ID: 'Group ID is empty',
   EMPTY_GROUP_NAME: 'Group name is empty',
+  NULL_GROUP: 'Group is null',
   UNSUPPORTED_OPERATION: 'Unsupported operation'
 };
