72962cd3a399eabeca868a33f2df19463775b7b2
Keerthikan@Home <kk.devpro11@gmail.com>
2017-05-07 20:26:49 +0200

    separate GroupSpy and PersonSpy objects

---
+++ src/test/spy-factory/group-spy-factory.ts
@@ -0,0 +1,25 @@
+import {Observable} from 'rxjs/Observable';
+import {GroupTestData} from '../data/group-test-data';
+/**
+ * Created by Keerthikan on 07-May-17.
+ */
+
+export let groupSpyFactory = (jasmine) => {
+  const groupSpy = jasmine.createSpyObj('groupResourceService',
+    ['createGroup', 'getGroup', 'deleteGroup']);
+
+  groupSpy.createGroup.and.callFake((name) => {
+    return Observable.of<any>({id: '1234', name});
+  });
+
+  // DAL should return a Group DTO
+  groupSpy.getGroup.and.callFake((id) => {
+    const group = GroupTestData.group;
+    group.id = id;
+    return Observable.of<any>(group);
+  });
+
+  groupSpy.deleteGroup.and.callFake((id) => Observable.of<boolean>(true));
+
+  return groupSpy;
+};
