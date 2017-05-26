781e97a7ab857073f901c49aa0573e8bf557076c
thde <d@thde.io>
2017-05-26 12:17:22 +0200

    add scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import {Component} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'tylr-root',
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    router.events.subscribe((val) => {
+      if (val instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
